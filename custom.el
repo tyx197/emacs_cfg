1936bf57fff37855014fab9f806d23f1d780609f
tyx197 <ytaoai@connect.ust.hk>
2023-12-31 22:24:32 +0800

    add new init

---
+++ custom.el
@@ -0,0 +1,23 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   '(centaur-tabs all-the-icons gruvbox-theme verilog-mode counsel))
+ '(verilog-auto-indent-on-newline nil)
+ '(verilog-auto-newline nil)
+ '(verilog-case-indent 4)
+ '(verilog-cexp-indent 0)
+ '(verilog-highlight-grouping-keywords t)
+ '(verilog-indent-level 4)
+ '(verilog-indent-level-behavioral 4)
+ '(verilog-indent-level-declaration 0)
+ '(verilog-indent-level-directive 4)
+ '(verilog-indent-level-module 0))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
